44746aa3fb22f3f1c5db92eb75b5aed6dd2134fb
Harper Frankstone <Harper Frankstone>
2017-02-01 23:01:59 -0600

    chaining methods complete

--- chaining.js
+++ chaining.js
@@ -9,9 +9,14 @@ Then output (either in the DOM or the console) the sum of all the resulting numb
 
 */
 
-var sorted = integers.sort(function(a, b){return b - a}).splice(4, 10).map(function(num){return (num * 1.5) - 1});
-// var taken = sorted;
-// var multiplied = sorted;
-console.log("Array sorted in descending order: ", sorted);
-console.log("Integers greater than 19 have been taken off: ", sorted);
-console.log("Each number has been multiplied by 1.5: ", sorted);
+var doneZo = integers.sort(function(a, b){return b - a}).splice(4, 10).map(function(num){return (num * 1.5) - 1}).reduce(function(a, b){return a + b});
+console.log("The finished product: ", doneZo);
+// var sorted = integers.sort(function(a, b){return b - a}).splice(4, 10).map(function(num){return (num * 1.5) - 1}).reduce(function(a, b){return a + b});
+// var taken = sorted.splice(4, 10);
+// var multiplied = sorted.map(function(num){return (num * 1.5) - 1});
+// var sumOfAll = sorted.reduce(function(a, b){return a + b});
+// console.log("Array sorted in descending order: ", sorted);
+// console.log("Integers greater than 19 have been taken off: ", taken);
+// console.log("Each number has been multiplied by 1.5: ", multiplied);
+// console.log("The sum of each of these numbers: ", sumOfAll);
+// console.log("The array has been sorted, shrunk, multiplied, subtracted from, and sumed up: ", doneZo);
